e933af26e7a02c265a0986e798f7c0f10c2dc7db
Andey Robins <ja548335@ucf.edu>
2024-02-04 18:48:30 -0500

    Update experiments with seed and echo

--- docs/benchmarks/experiment.sh
+++ docs/benchmarks/experiment.sh
@@ -1,8 +1,19 @@
-# iterature through each .blif file in the directory and run the command to evolution capture data
+go build
+
+echo "Running for epsilon 50"
 for file in ./docs/benchmarks/blif/*.blif; do
-    ./magical -evolve -graph $file -out ./docs/benchmarks/sequences/$(basename $file .blif)_p2000_e500_mu20_s1.seq -pop 2000 -epsilon 500 -mutation 0.2 -verbose 2> ./docs/benchmarks/sequences/$(basename $file .blif)_p2000_e500_mu20_s1.out
+    echo $file
+    ./magical -evolve -graph $file -out ./docs/benchmarks/sequences/$(basename $file .blif)_p2000_e50_mu20_s2.seq -seed 2 -pop 2000 -epsilon 50 -mutation 0.2 -verbose 2> ./docs/benchmarks/sequences/$(basename $file .blif)_p2000_e50_mu20_s1.out
 done
 
+echo "Running for epsilon 500"
 for file in ./docs/benchmarks/blif/*.blif; do
-    ./magical -evolve -graph $file -out ./docs/benchmarks/sequences/$(basename $file .blif)_p2000_e5000_mu20_s1.seq -pop 2000 -epsilon 5000 -mutation 0.2 -verbose 2> ./docs/benchmarks/sequences/$(basename $file .blif)_p2000_e5000_mu20_s1.out
+    echo $file
+    ./magical -evolve -graph $file -out ./docs/benchmarks/sequences/$(basename $file .blif)_p2000_e500_mu20_s2.seq -seed 2 -pop 2000 -epsilon 500 -mutation 0.2 -verbose 2> ./docs/benchmarks/sequences/$(basename $file .blif)_p2000_e500_mu20_s1.out
+done
+
+echo "Running for epsilon 5000"
+for file in ./docs/benchmarks/blif/*.blif; do
+    echo $file
+    ./magical -evolve -graph $file -out ./docs/benchmarks/sequences/$(basename $file .blif)_p2000_e5000_mu20_s2.seq -seed 2 -pop 2000 -epsilon 5000 -mutation 0.2 -verbose 2> ./docs/benchmarks/sequences/$(basename $file .blif)_p2000_e5000_mu20_s1.out
 done
